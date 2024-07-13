fdcee7399c08e4a4cc982fc920cae793ebdf7177
Asadbek27Code <arashi27asadbek@gmail.com>
2024-07-13 12:42:01 +0500

    commit by asadbek

--- lib/src/core/routes/go_route_system.dart
+++ lib/src/core/routes/go_route_system.dart
@@ -1,27 +1,15 @@
 import 'package:eduflix_mobile_test/src/core/routes/app_route_name.dart';
+import 'package:eduflix_mobile_test/src/feature/auth/view/pages/onboarding_page.dart';
 import 'package:eduflix_mobile_test/src/feature/certificate/view/pages/certificate_page.dart';
 import 'package:eduflix_mobile_test/src/feature/course/view/pages/course_page.dart';
 import 'package:eduflix_mobile_test/src/feature/learning/view/pages/learning_page.dart';
 import 'package:go_router/go_router.dart';
 
-final class GoRouterApp{
-  static GoRouter router = GoRouter(
-    initialLocation: AppRouteName.certificatePage,
-    routes: [
-      GoRoute(
-        path: AppRouteName.coursePage,
-        builder: (context, state)=> CoursePage()
-      ),
-
-      GoRoute(
-          path: AppRouteName.learningPage,
-          builder: (context, state)=> const LearningPage()
-      ),
-
-      GoRoute(
-          path: AppRouteName.certificatePage,
-          builder: (context, state)=> const CertificatePage()
-      )
-    ]
-  );
+final class GoRouterApp {
+  static GoRouter router = GoRouter(initialLocation: AppRouteName.onBoardingPage, routes: [
+    GoRoute(path: AppRouteName.coursePage, builder: (context, state) => CoursePage()),
+    GoRoute(path: AppRouteName.onBoardingPage, builder: (context, state) => OnboardingPage()),
+    GoRoute(path: AppRouteName.learningPage, builder: (context, state) => const LearningPage()),
+    GoRoute(path: AppRouteName.certificatePage, builder: (context, state) => const CertificatePage())
+  ]);
 }
